3b358d2404dbbe99c4b8d486930d09eae27ca93e
nok1214 <80727744+nok1214@users.noreply.github.com>
2023-03-08 21:08:43 -0600

    initial commit, 2 files modified, 4 added

---
+++ src/components/CarForm.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CarForm() {
+  return <div>CarForm</div>;
+}
---
+++ src/components/CarSearch.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CarSearch() {
+  return <div>CarSearch</div>;
+}

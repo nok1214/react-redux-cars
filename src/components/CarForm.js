fe06741f23eac8444738bff7c31522a30f735ff4
nok1214 <80727744+nok1214@users.noreply.github.com>
2023-03-08 22:12:08 -0600

    added carsSlice, formSlice, connected store to the App, edited CarForm component

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,5 +1,47 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeName, changeCost } from '../store';
 
 export default function CarForm() {
-  return <div>CarForm</div>;
+  const dispatch = useDispatch();
+  const { name, cost } = useSelector((state) => {
+    return {
+      name: state.form.name,
+      cost: state.form.cost,
+    };
+  });
+  const handleNameChange = (e) => {
+    dispatch(changeName(e.target.value));
+  };
+
+  const handleCostChange = (e) => {
+    const carCost = parseInt(e.target.value) || 0;
+    dispatch(changeCost(carCost));
+  };
+  return (
+    <div className="car-form panel">
+      <h4 className="subtitle is-3">Add car</h4>
+      <form>
+        <div className="field-group">
+          <div className="field">
+            <label className="label">Name</label>
+            <input
+              className="input is-expanded"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </div>
+          <div className="field">
+            <label className="label">Cost</label>
+            <input
+              className="input is-expanded"
+              value={cost}
+              onChange={handleCostChange}
+              type="number"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
+import { Provider } from 'react-redux';
+import { store } from './store';
 import App from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
